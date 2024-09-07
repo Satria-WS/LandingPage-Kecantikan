b4ae7382f296499126a0af6e49d5d1f5d9b451fb
Satria-WS <extencezone02@gmail.com>
2024-09-07 09:04:19 +0700

    add beranda component

---
+++ src/app/components/beranda/beranda.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-beranda',
+  standalone: true,
+  imports: [],
+  templateUrl: './beranda.component.html',
+  styleUrl: './beranda.component.css'
+})
+export class BerandaComponent {
+
+}

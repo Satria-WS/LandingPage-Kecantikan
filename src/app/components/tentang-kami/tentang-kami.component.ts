897e319fb6d7b3d573f001acd6296cf591d4eb92
Satria-WS <extencezone02@gmail.com>
2024-09-07 09:05:55 +0700

    add tentang-kami component

---
+++ src/app/components/tentang-kami/tentang-kami.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tentang-kami',
+  standalone: true,
+  imports: [],
+  templateUrl: './tentang-kami.component.html',
+  styleUrl: './tentang-kami.component.css'
+})
+export class TentangKamiComponent {
+
+}

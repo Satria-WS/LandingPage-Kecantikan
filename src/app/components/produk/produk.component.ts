43345225817ce4dc1e720a2bcebdd4895e5db72b
Satria-WS <extencezone02@gmail.com>
2024-09-07 09:05:35 +0700

    add produk component

---
+++ src/app/components/produk/produk.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-produk',
+  standalone: true,
+  imports: [],
+  templateUrl: './produk.component.html',
+  styleUrl: './produk.component.css'
+})
+export class ProdukComponent {
+
+}

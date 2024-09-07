47856427f74fbc0045243c0dcb88221b40d9a60d
Satria-WS <extencezone02@gmail.com>
2024-09-07 09:04:59 +0700

    add peluang bisnis

---
+++ src/app/components/peluang-bisnis/peluang-bisnis.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-peluang-bisnis',
+  standalone: true,
+  imports: [],
+  templateUrl: './peluang-bisnis.component.html',
+  styleUrl: './peluang-bisnis.component.css'
+})
+export class PeluangBisnisComponent {
+
+}

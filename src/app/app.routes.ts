f7712cbf5bc58e621fceaabbada6798953e25e58
Satria-WS <extencezone02@gmail.com>
2024-09-10 20:22:12 +0700

    update route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,36 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TentangKamiComponent } from './components/tentang-kami/tentang-kami.component';
+import { ProdukComponent } from './components/produk/produk.component';
+import { PeluangBisnisComponent } from './components/peluang-bisnis/peluang-bisnis.component';
+import { BerandaComponent } from './components/beranda/beranda.component';
 
 export const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'beranda',
+        pathMatch: 'full',
+      },
+      {
+        path: 'beranda',
+        component: BerandaComponent,
+      },
+      {
+        path: 'tentang-kami',
+        component: TentangKamiComponent,
+      },
+      {
+        path: 'produk',
+        component: ProdukComponent,
+      },
+      {
+        path: 'peluang-bisnis',
+        component: PeluangBisnisComponent,
+      },
+    ],
   },
 ];

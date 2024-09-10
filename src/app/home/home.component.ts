9bcf5759990ae0f580616e81ed5fb81d9792ff11
Satria-WS <extencezone02@gmail.com>
2024-09-10 20:23:46 +0700

    update home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/header/header.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent, RouterOutlet, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+  logo = '/assets/logo.svg';
+}

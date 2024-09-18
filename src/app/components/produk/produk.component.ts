ca318b56dccd09c5dbec41265d8ff11590435dcc
Satria-WS <extencezone02@gmail.com>
2024-09-18 16:38:55 +0700

    update component peluang-bisnis

--- src/app/components/produk/produk.component.ts
+++ src/app/components/produk/produk.component.ts
@@ -7,4 +7,33 @@ import { Component } from '@angular/core';
   templateUrl: './produk.component.html',
   styleUrl: './produk.component.css',
 })
-export class ProdukComponent {}
+export class ProdukComponent {
+
+  image = [
+    {
+    name:"image-1",
+    src:"/assets/produk/image-1.png"
+    },
+    {
+      name:"image-2",
+      src:"/assets/produk/image-2.png"
+    },
+    {
+      name:"image-3",
+      src:"/assets/produk/image-3.png"
+    },
+    {
+      name:"image-4",
+      src:"/assets/produk/image-4.png"
+    },
+    {
+      name:"image-5",
+      src:"/assets/produk/image-5.png"
+    },
+    {
+      name:"image-6",
+      src:"/assets/produk/image-6.png"
+     },
+]
+
+}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../components/header/header.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, RouterLink],
+  imports: [HeaderComponent, RouterOutlet, RouterLink , FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })

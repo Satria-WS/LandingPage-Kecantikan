555234b5f921ed386348964d949e87b54652e4be
Satria-WS <extencezone02@gmail.com>
2024-09-26 10:20:50 +0700

    prettierrc

--- src/app/components/produk/produk.component.ts
+++ src/app/components/produk/produk.component.ts
@@ -8,32 +8,30 @@ import { Component } from '@angular/core';
   styleUrl: './produk.component.css',
 })
 export class ProdukComponent {
-
   image = [
     {
-    name:"image-1",
-    src:"/assets/produk/image-1.png"
+      name: 'image-1',
+      src: '/assets/produk/image-1.png',
     },
     {
-      name:"image-2",
-      src:"/assets/produk/image-2.png"
+      name: 'image-2',
+      src: '/assets/produk/image-2.png',
     },
     {
-      name:"image-3",
-      src:"/assets/produk/image-3.png"
+      name: 'image-3',
+      src: '/assets/produk/image-3.png',
     },
     {
-      name:"image-4",
-      src:"/assets/produk/image-4.png"
+      name: 'image-4',
+      src: '/assets/produk/image-4.png',
     },
     {
-      name:"image-5",
-      src:"/assets/produk/image-5.png"
+      name: 'image-5',
+      src: '/assets/produk/image-5.png',
     },
     {
-      name:"image-6",
-      src:"/assets/produk/image-6.png"
-     },
-]
-
+      name: 'image-6',
+      src: '/assets/produk/image-6.png',
+    },
+  ];
 }

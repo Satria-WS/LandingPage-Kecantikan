f04d7d73026bb76088313865d7918e499cb35a67
Satria-WS <extencezone02@gmail.com>
2024-09-26 10:21:10 +0700

    prettierrc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,7 +6,7 @@ import { FooterComponent } from '../components/footer/footer.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, RouterOutlet, RouterLink , FooterComponent],
+  imports: [HeaderComponent, RouterOutlet, RouterLink, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
